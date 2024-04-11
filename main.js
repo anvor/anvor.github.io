0ab91d6f7a657635a40247820453c8b6fc7d3b07
anvor <vorobey94ant@googlemail.com>
2024-04-11 17:54:42 +0200

    fixing bootstrap resolution

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './styles.scss'
-import './bootstrap/dist/css/bootstrap.css'
-import './bootstrap/dist/js/bootstrap.js'
+import './node_modules/bootstrap/dist/css/bootstrap.css'
+import './node_modules/bootstrap/dist/js/bootstrap.js'
 import './assets/css/main.css'
 
 // Skills section - selecting a skill will display a description of that skill
